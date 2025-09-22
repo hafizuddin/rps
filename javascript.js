7aafe3849735e5ff5241215fc2add5d3b3150719
hafizuddin <41276836+hafizuddin@users.noreply.github.com>
2025-09-23 06:48:10 +0800

    Finished the logical part, using button Will come back to make it pretty for now the logic part is finish just need to make it look good

--- javascript.js
+++ javascript.js
@@ -1,50 +1,75 @@
-let humanScore = 0;
+// ---- Global State ---
+let playerScore = 0;
 let computerScore = 0;
 
-function translate(num){
-    if (num == 1){
-        return "Rock";
-    }else if (num == 2){
-        return "Paper";
-    }else if (num == 3){
-        return "Scissor";
+// --- Selectors ---
+const result = document.querySelector(".result");
+const buttons = document.querySelectorAll(".button-container button");
+const playAgain = document.querySelector(".again");
+const player = document.querySelector(".player");
+const computer = document.querySelector(".computer");
+
+
+// --- Event listeners ---
+buttons.forEach((button) => {
+    button.addEventListener("click", (event) => gameStart(event));
+});
+
+playAgain.addEventListener("click", resetGame);
+
+// --- Functions ---
+function gameStart(event){
+    const playerChoice = event.target.className;     // className if class ="rock class2 class3" it will return string of class instead of array of class
+    player.textContent = "Player " + playerChoice; // safer option is classList[0] = it will get rock only as it is the first class 
+
+    const computerChoice = getComputerChoice();
+    computer.textContent = "Computer " + computerChoice;
+
+    playRound(playerChoice, computerChoice);
+
+    result.textContent = "Player " + playerScore + " : " + computerScore + " Computer";
+
+    if (playerScore + computerScore === 5){
+        endGame();
     }
 }
 
 function getComputerChoice(){
     let comp = Math.random();
-    if (comp <= 0.3){
-        return 1;
-    }else if (comp > 0.3 & comp <= 0.6){
-        return 2;
-    }else{
-        return 3;
-    }
+    if (comp <= 0.33) return "ROCK";
+    else if (comp <= 0.66) return "PAPER";
+    else return "SCISSOR";
 }
 
-function getHumanChoice(){
-    let human = Number(prompt("1. Rock\n 2. Paper\n 3. Scissor"));
-    return human;
-}
-
-function playRound(humanChoice=getHumanChoice(), computerChoice=getComputerChoice()){
-    if(humanChoice == computerChoice){
-        return "Draw! " + translate(humanChoice) + " & " + translate(computerChoice);
-    }else if ((humanChoice == 1 && computerChoice == 3) || (humanChoice == 2 && computerChoice == 1) || (humanChoice == 3 && computerChoice == 2)) {
-        ++humanScore;
-        return "You Win! " + translate(humanChoice) + " beats " + translate(computerChoice);
+function playRound(playerChoice, computerChoice){
+    if(playerChoice == computerChoice) return;
+    if (
+        (playerChoice == "ROCK" && computerChoice == "SCISSOR") || 
+        (playerChoice == "PAPER" && computerChoice == "ROCK") || 
+        (playerChoice == "SCISSOR" && computerChoice == "PAPER")
+    ) {
+        playerScore++;
     }else {
-        ++computerScore;
-        return "You Lose! " + translate(computerChoice) + " beats " + translate(humanChoice);
+        computerScore++;
     }
 }
 
-function playGame(){
-    for(i=0; i<5; i++){
-        console.log(playRound());
-        console.log("Score: Human = " + humanScore +" Computer = " + computerScore);
+function endGame(){
+    if (playerScore > computerScore) {
+        result.textContent = "You win! " + playerScore + " : " + computerScore;
+    } else {
+        result.textContent = "You lose! " + playerScore + " : " + computerScore;
     }
-    return "Final Score: Human = " + humanScore +" Computer = " + computerScore;
+
+    buttons.forEach((button) => (button.disabled = true));
 }
 
-console.log(playGame());
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    player.textContent = "Player: ";
+    computer.textContent = "Computer: ";
+    result.textContent = "New game started!";
+    
+    buttons.forEach((button) => (button.disabled = false));
+}
